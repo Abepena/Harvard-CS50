96aa09f887969b782e5ff65887e99edaa27f8690
abepena <pena.abe@gmail.com>
2018-07-16 13:42:26 -0400

    book detail page added

--- project1/app.py
+++ project1/app.py
@@ -1,7 +1,6 @@
 import os
 import requests
-from flask import (Flask, session, render_template,
-                     url_for, redirect, request, flash)
+from flask import Flask, session, render_template, url_for, redirect, request, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -25,11 +24,12 @@ db = scoped_session(sessionmaker(bind=engine))
 # session["username"] = None
 # session["logged_in"] = False
 
+
 @app.route("/", methods=["GET"])
 def index():
     if session.get("username") == None:
         return render_template("welcome.html")
-    return redirect(url_for('search'))
+    return redirect(url_for("search"))
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -42,7 +42,9 @@ def register():
             ).rowcount
             > 0
         ):
-            return render_template(url_for("register"), message="That username has already been taken!")
+            return render_template(
+                url_for("register"), message="That username has already been taken!"
+            )
         else:
             email = request.form["email"]
             password_hash = generate_password_hash(request.form["password"])
@@ -60,13 +62,13 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if session.get("username") != None:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
         stored_user_data = db.execute(
             "SELECT username, password_hash from users WHERE username= :username",
-            {"username": username}
+            {"username": username},
         ).fetchone()
         password_match = check_password_hash(stored_user_data.password_hash, password)
 
@@ -76,43 +78,58 @@ def login():
             return render_template(url_for("login"), error_msg="Incorrect password")
         else:
             session["username"] = username
-            return redirect(url_for('search'))
+            return redirect(url_for("search"))
     return render_template("login.html")
+
 
 @app.route("/logout")
 def logout():
-    session.pop('username')
+    session.pop("username")
     return redirect(url_for("index"))
 
-@app.route("/books")
-def books():
-    if session["username"]:
-        book_list = db.execute("SELECT isbn, title, author, year FROM books").fetchall()
-        return render_template("book_list.html", book_list=book_list)
 
-@app.route('/search')
+@app.route("/search")
 def search():
-    return render_template('book_search.html')
+    return render_template("book_search.html")
 
-@app.route('/results/', methods=["POST"])
+
+@app.route("/results/", methods=["POST"])
 def results():
     search = request.form["search"]
+    if search == "":
+        return redirect(url_for("books"))
     try:
         year = int(search)
-        results = db.execute(
-            "SELECT * FROM books WHERE year=:year",
-            {"year": year}
-        )
+        results = db.execute("SELECT * FROM books WHERE year=:year", {"year": year})
     except:
         results = db.execute(
             "SELECT * from books WHERE\
             isbn LIKE :search OR author LIKE :search OR title LIKE :search",
-            {"search":"%" + search + "%"}
+            {"search": "%" + search + "%"},
         ).fetchall()
-    
+
     if results:
-        return render_template('book_list.html', book_list=results)
+        return render_template("book_list.html", book_list=results)
     return "No books in the database match your results"
+
+
+@app.route("/books")
+def books():
+    if session["username"]:
+        book_list = db.execute(
+            "SELECT isbn, title, author, year, id  FROM books"
+        ).fetchall()
+        return render_template("book_list.html", book_list=book_list)
+
+
+@app.route("/results/<int:book_id>")
+def book_detail(book_id):
+    book = db.execute(
+        "SELECT isbn, title, author, year, id FROM books WHERE id=:book_id",
+        {"book_id": book_id}
+    ).fetchone()
+    return render_template("book_detail.html", book=book)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
