233f989b46b51cca77cc7eedd0ccbae9d7fef2af
abepena <pena.abe@gmail.com>
2018-07-19 03:03:58 -0400

    added source code for lecture 4

---
+++ src4-orms-apis/airline4/models.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Flight(db.Model):
+    __tablename__ = "flights"
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String, nullable=False)
+    destination = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    #lazy = True means to not fetch the passengers information unless strictly
+    #looking for it. You can query the database lazily and not return any
+    #passenger information. the self.passengers property wont be shown unless
+    #explicitly called for
+    #this makes extracting all of the passengers easier
+    passengers = db.relationship("Passenger", backref="flight", lazy=True) #new
+    
+    def add_passenger(self, name):
+        p = Passenger(name=name, flight_id=self.id)
+        db.session.add(p)
+        db.session.commit()
+
+
+class Passenger(db.Model):
+    __tablename__ = "passengers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False)
